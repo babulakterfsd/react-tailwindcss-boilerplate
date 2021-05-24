318c4caf2531bd4e093701e6911eadee362e3c86
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-05-24 16:59:33 +0600

    React + Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="heading">
+        Hello From <span className="highlight">React + Tailwind!</span>
+      </h2>
+      <p className="text">A Quick React and Tailwind CSS Starter.</p>
+      <p className="text">
+        Edit <span className="highlight">src/App.jsx</span>
+      </p>
+    </div>
+  );
+}
+
+export default App;
