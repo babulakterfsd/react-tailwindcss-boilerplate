db1b3ba65ad7adc3e08c952f961b21a9b3f3aa6d
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-06-04 03:03:39 +0600

    React + Tailwindcss

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,9 @@
+/* eslint-disable global-require */
 module.exports = {
     style: {
-      postcss: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
-      },
+        postcss: {
+            // eslint-disable-next-line import/no-extraneous-dependencies
+            plugins: [require('tailwindcss'), require('autoprefixer')],
+        },
     },
-  }
+};
--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h2 className="heading">
-        Hello From <span className="highlight">React + Tailwind!</span>
-      </h2>
-      <p className="text">A Quick React and Tailwind CSS Starter.</p>
-      <p className="text">
-        Edit <span className="highlight">src/App.jsx</span>
-      </p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2 className="heading">
+                Hello From <span className="highlight">React + Tailwind!</span>
+            </h2>
+            <p className="text">A Quick React and Tailwind CSS Starter with ESLint Formatter.</p>
+            <p className="text">
+                Edit <span className="highlight">src/App.jsx</span>
+            </p>
+        </div>
+    );
 }
 
 export default App;
